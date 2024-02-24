b60b7ea029b568c74a31013e2dc68e764e3746ee
mick-25 <158113781+mick-25@users.noreply.github.com>
2024-02-24 17:04:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -115,14 +115,22 @@ let fetchTorrent = async (query) => {
     .then(async (results) => {});
 };
 
-function getMeta(id, type) {
-  var [tt, s, e] = id.split(":");
+async function getMeta(id, type) {
+  const [tt, s, e] = id.split(":");
+  const query = type === "series" ? tt : `${tt} ${s} ${e}`;
+
+  const shows = await getShowFromDCool(query, type);
+
+  if (shows.length > 0) {
+    return {
+      name: shows[0].title,
+      year: shows[0].year,
+    };
+  }
 
   return fetch(`https://v2.sg.media-imdb.com/suggestion/t/${tt}.json`)
     .then((res) => res.json())
-    .then((json) => {
-      return json.d[0];
-    })
+    .then((json) => json.d[0])
     .then(({ l, y }) => ({ name: l, year: y }))
     .catch((err) =>
       fetch(`https://v3-cinemeta.strem.io/meta/${type}/${tt}.json`)
@@ -133,6 +141,7 @@ function getMeta(id, type) {
         }))
     );
 }
+
 
 async function getImdbFromKitsu(id) {
   var [kitsu, _id, e] = id.split(":");
